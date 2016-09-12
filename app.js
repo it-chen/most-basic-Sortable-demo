2c9b1eac99ebfb57c6512d95fe88bd5d89d7cce7
it-chen <it_chen@126.com>
2016-09-12 11:21:34 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+
+
+  Sortable.create(document.getElementById('foo'), {
+    group: "aaa",
+    animation: 150
+  });
+
+
+  Sortable.create(document.getElementById('bar'), {
+    group: "aaa",
+    animation: 150
+  });
+
+
+
+})();
+
+
+
